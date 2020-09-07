4c0def3db57070cd613266884fef30cefc854b9f
root <root@admin>
2020-09-08 00:44:13 +0200

    git init

---
+++ assets/js/master.js
@@ -0,0 +1,24 @@
+function mouseStatus() {
+
+  onmousemove = function(m){
+    //console.log("mouse location:", m.clientX, m.clientY)
+    document.getElementById('mouseTracker').style.visibility = 'unset'
+    document.getElementById('mouseTracker').style.left = m.clientX+4+'px'
+    document.getElementById('mouseTracker').style.top = m.clientY+5+'px'
+  }
+  onmousedown = function(d){
+    document.getElementById("mouseTracker").src = "/assets/images/jamesheadOpenMouth.png"
+  }
+  onmouseup = function(u){
+    document.getElementById("mouseTracker").src = "/assets/images/jameshead.png"
+  }
+
+  document.getElementById("menu").onmouseover = function(o){
+    document.getElementById("mouseTracker").classList.add('rotating')
+  }
+  document.getElementById("menu").onmouseout = function(mo){
+    document.getElementById("mouseTracker").classList.remove('rotating')
+  }
+
+}
+mouseStatus()
